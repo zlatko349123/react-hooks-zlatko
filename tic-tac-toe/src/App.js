34d9abc7b8b9e640f45f8503f3d8615367c23148
zlatko349 <zlatko.kozic13@gmail.com>
2021-02-03 13:58:23 +0100

    First Commit

---
+++ tic-tac-toe/src/App.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import Game from './components/Game';
+
+const App = () => <Game />
+export default App;
---
+++ to-do/src/App.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import './App.css';
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
+
+function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState('all');
+  const [filteredTodos, setFilteredTodos] = useState([]);
+  useEffect(() => {
+    filterHandler();
+  }, [todos, status]);
+  const filterHandler = () => {
+    switch(status){
+      case 'completed':
+        setFilteredTodos(todos.filter(todo => todo.completed === true));
+        break;
+        case 'uncompleted':
+          setFilteredTodos(todos.filter(todo => todo.completed === false));
+          break;  
+          default:
+            setFilteredTodos(todos);
+            break;
+    }
+  };
+  return (
+    <div className="App">
+  <header><h1>zlatko's todo list</h1></header>
+    <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText} 
+      setStatus={setStatus} 
+    />
+    <TodoList filteredTodos={filteredTodos} setTodos={setTodos} todos={todos} />
+    </div>
+  );
+}
+
+export default App;
